adf0b57b148052c147ddfcf0910f1665f21948d4
igusa <henry12.henry14@gmail.com>
2018-02-07 01:48:53 +0900

    "   "

--- choose_photos/CameraRollCollectionViewCell.swift
+++ choose_photos/CameraRollCollectionViewCell.swift
@@ -2,7 +2,7 @@
 //  CameraRollCollectionViewCell.swift
 //  Gajumal
 //
-//  Created by 井草俊輔 on 2018/02/06.
+//  Created by 井草俊輔 on 2018/02/07.
 //  Copyright © 2018年 井草俊輔. All rights reserved.
 //
 
@@ -15,22 +15,8 @@ class CameraRollCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     // 画像を表示する
-    func setConfigure(assets: PHAsset) {
-        let manager = PHImageManager()
-        manager.requestImage(for: assets,
-                             targetSize: frame.size,
-                             contentMode: .aspectFill,
-                             options: nil,
-                             resultHandler: { [weak self] (image, info) in
-                                guard let wself = self, let outImage = image else {
-                                    return
-                                }
-                                wself.photoImageView.image = image
-        })
-    }
-
+    
 }
