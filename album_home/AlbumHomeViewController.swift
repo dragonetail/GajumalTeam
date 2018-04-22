216d972ef75a3ca82dcc8c49f8ad42b01c894265
igusa <henry12.henry14@gmail.com>
2018-04-22 23:40:13 +0900

    "ホームのレイアウトにボタンを追加"

--- album_home/AlbumHomeViewController.swift
+++ album_home/AlbumHomeViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
+/// ノード上に見せるメインの画面
 class AlbumHomeViewController: UIViewController, OnClickDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var basicHeader: BasicHeader!
+    @IBOutlet weak var plusButton: UIButton!
     
     
     override func viewDidLoad() {
@@ -19,11 +21,13 @@ class AlbumHomeViewController: UIViewController, OnClickDelegate {
         basicHeader.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UINib(nibName: "AlbumTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
+        tableView.register(UINib(nibName: "LeftNodeViewCell", bundle: nil), forCellReuseIdentifier: "leftCell")
+        tableView.register(UINib(nibName: "RightNodeViewCell", bundle: nil), forCellReuseIdentifier: "rightCell")
         tableView.separatorInset = UIEdgeInsets.zero
         tableView.separatorStyle = .none
-        
+        self.view.bringSubview(toFront: plusButton)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -33,23 +37,32 @@ class AlbumHomeViewController: UIViewController, OnClickDelegate {
     }
     
     func onClick(_ sender: AnyObject?) {
-        print("centerrrrrrrrrrrrrrrrrrrrrrr")
+        print("aaaa")
     }
-    
 }
 
 extension AlbumHomeViewController : UITableViewDelegate {
-    
 }
 
 extension AlbumHomeViewController : UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! AlbumTableViewCell
-        return cell
+        if indexPath.row == 0 || indexPath.row % 2 == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "leftCell") as! LeftNodeViewCell
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "rightCell") as! RightNodeViewCell
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
 }
---
+++ album_home/LeftNodeViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  LeftNodeViewCell.swift
+//  Gajumal
+//
+//  Created by 井草俊輔 on 2018/04/21.
+//  Copyright © 2018年 井草俊輔. All rights reserved.
+//
+
+import UIKit
+
+class LeftNodeViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var arrowView: UIView!
+    @IBOutlet weak var topView: UIView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customInit()
+        makeTriangle()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func customInit() {
+//        let layerView = UIView(frame: CGRect(x: 0, y: 0, width: mainView.frame.size.width, height: mainView.frame.size.height))
+//        layerView.center = mainView.center
+//        layerView.layer.borderWidth = 1.0
+        let rgb = UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0)
+        //layerView.layer.borderColor = UIColor.lightGray.cgColor
+//        layerView.layer.borderColor = rgb.cgColor
+//        mainView.addSubview(layerView)
+        topView.layer.borderColor = UIColor.lightGray.cgColor
+        topView.layer.borderWidth = 1.0
+    }
+    
+    func makeTriangle() {
+        let triangleLayer = CAShapeLayer.init()
+        let triangleFrame = CGRect.init(x: 0, y: 0, width: arrowView.frame.size.width, height: arrowView.frame.size.height)
+        triangleLayer.frame = triangleFrame
+        
+        // 輪郭の色
+        triangleLayer.strokeColor = UIColor.lightGray.cgColor
+        // 三角形の中の色
+        let rgb = UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0)
+        triangleLayer.fillColor = rgb.cgColor
+        // 輪郭の太さ
+        triangleLayer.lineWidth = 1.0
+        
+        // 三角形を描画
+        let triangleLine = UIBezierPath()
+        triangleLine.move(to: CGPoint.init(x: 0, y: triangleFrame.size.height))
+        triangleLine.addLine(to: CGPoint.init(x: triangleFrame.size.width / 2, y: 0))
+        triangleLine.addLine(to: CGPoint.init(x: triangleFrame.size.width, y: triangleFrame.size.height))
+        triangleLine.close()
+        
+        // 三角形を描画
+        triangleLayer.path = triangleLine.cgPath
+        arrowView.layer.addSublayer(triangleLayer)
+        arrowView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 90.0 / 180.0));
+    }
+    
+}
