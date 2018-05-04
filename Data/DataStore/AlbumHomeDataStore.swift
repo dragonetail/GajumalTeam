8b1c9e2cd80375c95c33c19d24ed451196bb0eff
igusa <henry12.henry14@gmail.com>
2018-05-05 02:41:26 +0900

    ""

---
+++ Data/DataStore/AlbumHomeDataStore.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumHomeDataStore.swift
+//  Gajumal
+//
+//  Created by 井草俊輔 on 2018/05/05.
+//  Copyright © 2018年 井草俊輔. All rights reserved.
+//
+
+import Foundation
+import Pods_Gajumal
+
+public protocol AlbumHomeDataStore {
+    
+}
+
+struct AlbumHomeDataStoreImpl {
+    let request: ApiClient = ApiClient()
+    
+}
---
+++ Data/Entity/AlbumHomeEntity.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumHomeEntity.swift
+//  Gajumal
+//
+//  Created by 井草俊輔 on 2018/05/05.
+//  Copyright © 2018年 井草俊輔. All rights reserved.
+//
+
+import Foundation
+
+public struct AlbumHomeEntity {
+    
+}
